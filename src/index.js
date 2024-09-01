5ff05f39282d03f982b459e88033978d3a9a2d75
NillsLopez <yakusaz17@gmail.com>
2024-09-01 19:36:00 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+//  import {render} from 'react-dom';
+//import { createRoot } from "react-dom/client";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
